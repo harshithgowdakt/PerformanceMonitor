c3726b7bf4a9a9697f83c1ffdd30c8be0e49f6be
Harshith Gowda <harshithgowdakt@gmail.com>
2023-03-30 09:49:26 +0530

    initial commit

---
+++ PerformanceMonitor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+
+namespace PerformanceMonitor
+{
+    class Program
+    {
+        protected static PerformanceCounter cpuCounter;
+        protected static PerformanceCounter ramCounter;
+        static void Main(string[] args)
+        {
+            cpuCounter = new PerformanceCounter("Process", "% Processor Time",Process.GetCurrentProcess().ProcessName);
+
+
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes", Process.GetCurrentProcess().ProcessName);
+
+            try
+            {
+                System.Timers.Timer t = new System.Timers.Timer(500);
+                t.Elapsed += new ElapsedEventHandler(TimerElapsed);
+                t.Start();
+                Thread.Sleep(10000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"catched exception {e}");
+            }
+            Console.ReadLine();
+
+        }
+
+        public static void TimerElapsed(object source, ElapsedEventArgs e)
+        {
+            float cpu = cpuCounter.NextValue();
+            float ram = ramCounter.NextValue();
+            Console.WriteLine("RAM: " + (ram /1024/1024) + " MB; CPU: " + (cpu) + " %");
+        }
+    }
+}
